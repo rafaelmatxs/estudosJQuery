020eb13d096520b5aeb4aa2ff583cc8ad261cbcb
rafaelmatxs <rafaelbragaodematos@hotmail.com>
2023-08-20 01:48:59 -0300

    Estudos jQuery

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() { //verifica se todos os elementos 
+                               //do body já estão no dom para 
+                               //entao montar o javascript
+
+    //o código javascript vem aqui dentro                               
+    console.log($('#exemplo'))
+
+    
+
+})
+
+
+
+    
